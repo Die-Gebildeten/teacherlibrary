717a249a9dfa2306fcb979ea71b7dab72af29189
EmilyCook321 <emilyacook16@gmail.com>
2020-11-05 11:22:28 +0100

    Add label, design not finalised

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import DownloadButton from './components/DownloadButton';
+import CardLabel from './components/CardLabel';
 
 
 function App() {
   return (
+    <div>
+    <CardLabel>Q1/Q2</CardLabel>
   <DownloadButton />
+  </div>
   );
 }
 
---
+++ src/components/CardLabel.js
@@ -0,0 +1,15 @@
+import React from "react";
+import emotion from "@emotion/styled";
+import styled from "@emotion/styled";
+
+const CardLabel = styled.div`
+  border-radius: 10px;
+  background-color: red;
+  text-align: center;
+  width: 60px;
+  height: 25px;
+  font-size: 18px;
+  padding-top: 3.5px;
+`;
+
+export default CardLabel;
