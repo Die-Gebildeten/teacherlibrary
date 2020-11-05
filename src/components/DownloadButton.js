ecf4a13ce83e7d6503033a3275f30dbf5e85dfb3
EmilyCook321 <emilyacook16@gmail.com>
2020-11-05 10:49:23 +0100

    Create download button

---
+++ src/components/DownloadButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import DownloadIcon from "../assets/icons/downloadicon.svg";
+import styled from "@emotion/styled";
+
+function DownloadButton(props) {
+  return (
+    <Button>
+      <img src={DownloadIcon} alt="Download Button" />
+    </Button>
+  );
+}
+
+export default DownloadButton;
+
+const Button = styled.button`
+width: 40px;
+height: 40px;
+border-radius: 50%;
+`;
