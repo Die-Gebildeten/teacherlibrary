8177125d9db636d08e489675ee7c0155374e483a
EmilyCook321 <emilyacook16@gmail.com>
2020-11-10 12:06:38 +0100

    Styled card section

--- src/pages/LessonPage.js
+++ src/pages/LessonPage.js
@@ -2,11 +2,29 @@ import React from "react";
 import { getLessons } from "../api/getLessons";
 import Card from "../components/Card";
 import useAsync from "../hooks/useAsync";
+import styled from "@emotion/styled";
+
+const CardSection = styled.section`
+  display: grid;
+  grid-template-columns: repeat(5, minmax(auto, 50px));
+  @media (min-width: 900px) {
+    grid-template-columns: repeat(10, minmax(auto, 50px));
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(15, minmax(auto, 50px));
+  }
+  grid-gap: 40px;
+  align-items: center;
+  justify-content: center;
+  & * {
+    grid-column: span 4;
+  }
+`;
 
 function LessonPage(props) {
   const { data: lessons } = useAsync(getLessons);
   return (
-    <div>
+    <CardSection>
       {lessons?.map((lesson) => (
         <Card
           id={lesson.id}
@@ -20,7 +38,7 @@ function LessonPage(props) {
           tag4={lesson.tag4}
         />
       ))}
-    </div>
+    </CardSection>
   );
 }
 
