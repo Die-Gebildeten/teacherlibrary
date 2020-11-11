057d64d8a9a85d591279fd4772c54b796a4ae3cf
sschoe85 <s@bine-schoetensack.eu>
2020-11-11 12:42:17 +0100

    Add basic routing and form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import Card from "../src/components/Card";
-import { getLessons } from "./api/getLessons";
+import React from "react";
+import AddLessonPage from "./pages/AddLessonPage";
 import LessonPage from "./pages/LessonPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-async function fetchLessons(){
-  const lessons = await getLessons();
-  console.log(lessons);
-}
-
-fetchLessons();
-
   return (
-    <>
-    <LessonPage />
-     
-    </>
+    <Router>
+      <Link to="/add">Add Lesson</Link>
+      <Switch>
+        <Route path="/add">
+          <AddLessonPage />
+        </Route>
+        <Route path = "/"><LessonPage /></Route>
+      </Switch>
+    </Router>
   );
 }
 
