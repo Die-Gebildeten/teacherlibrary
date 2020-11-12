d08c6b4b4e9690acb17c35b65cd4a050ae908fe7
sschoe85 <s@bine-schoetensack.eu>
2020-11-12 11:13:43 +0100

    Added character counters, added selected option to dropdown

--- src/pages/AddLessonPage.js
+++ src/pages/AddLessonPage.js
@@ -1,18 +1,26 @@
-
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function AddLessonPage() {
-   
-      
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, watch, errors, handleSubmit } = useForm();
+  const watchTitle = watch("title", "");
+  const watchDescription = watch("description", "");
+  const onSubmit = (data) => console.log(data);
   console.log(errors);
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Title" name="Title" ref={register({required: true, maxLength: 80 })} />
-      <select name="Subject" ref={register({ required: true })}>
+      <input
+        type="text"
+        placeholder="Title"
+        name="title"
+        ref={register({ required: true, maxLength: 80 })}
+      />
+      <p>You have {80 - watchTitle.length} characters left</p>
+      <select name="subject" ref={register({ required: true })}>
+        <option value="" selected disabled hidden>
+          choose subject
+        </option>
         <option value="Maths">Maths</option>
         <option value=" German"> German</option>
         <option value=" Chemistry"> Chemistry</option>
@@ -29,8 +37,17 @@ export default function AddLessonPage() {
         <option value=" Politics"> Politics</option>
         <option value=" English"> English</option>
       </select>
-      <textarea name="Description" placeholder="Description" ref={register({required: true, maxLength: 160})} />
-      <select name="Grade" ref={register({ required: true })}>
+      <textarea
+        name="description"
+        placeholder="Description"
+        ref={register({ required: true, maxLength: 160 })}
+      />
+      <p>You have {160 - watchDescription.length} characters left</p>
+
+      <select name="grade" ref={register({ required: true })}>
+        <option value="" selected disabled hidden>
+          choose grade
+        </option>
         <option value="1/2">1/2</option>
         <option value=" 3/4"> 3/4</option>
         <option value=" 5/6"> 5/6</option>
@@ -39,11 +56,32 @@ export default function AddLessonPage() {
         <option value=" 11"> 11</option>
         <option value=" 12"> 12</option>
       </select>
-      <input type="text" placeholder="tag1" name="tag1" ref={register({required: true, maxLength: 100})} />
-      <input type="text" placeholder="tag2" name="tag2" ref={register({maxLength: 100})} />
-      <input type="text" placeholder="tag3" name="tag3" ref={register({maxLength: 100})} />
-      <input type="text" placeholder="tag4" name="tag4" ref={register({maxLength: 100})} />
-      <input type="file" name="file" ref={register({required: true})}/>
+      <input
+        type="text"
+        placeholder="tag1"
+        name="tag1"
+        ref={register({ required: true, maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="tag2"
+        name="tag2"
+        ref={register({ maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="tag3"
+        name="tag3"
+        ref={register({ maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="tag4"
+        name="tag4"
+        ref={register({ maxLength: 100 })}
+      />
+      <input type="file" name="file" ref={register({ required: true })} />
       <input type="submit" />
-    </form>);
+    </form>
+  );
 }
